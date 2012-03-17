89902f495f631cb6ae75982e7c19e540ab76436e
Andrew Shull <andrew.shull1@gmail.com>
2012-03-17 10:57:03 -0400

    Hello world working.

---
+++ apps/namedapp/namedapp_list.h
@@ -0,0 +1,3 @@
+/* List of application requirements, generated during make context. */
+{ "hello", SCHED_PRIORITY_DEFAULT, 768, hello_main },
+{ "autopilotone", SCHED_PRIORITY_DEFAULT, 768, autopilotone_main },
---
+++ apps/namedapp/namedapp_proto.h
@@ -0,0 +1,3 @@
+/* List of application entry points, generated during make context. */
+EXTERN int hello_main(int argc, char *argv[]);
+EXTERN int autopilotone_main(int argc, char *argv[]);
